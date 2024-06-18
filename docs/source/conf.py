28703ec5a556173c1c845defa2542dc1a7a7a4b9
alberthli <alberthli@caltech.edu>
2024-06-17 23:43:28 -0700

    add incomplete docs because otherwise tests complain about formatting

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,28 +6,28 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'obelisk'
-copyright = '2024, AMBER Lab'
-author = 'AMBER Lab'
+project = "obelisk"
+copyright = "2024, AMBER Lab"
+author = "AMBER Lab"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    'myst_parser',
-    'sphinx_rtd_theme',
+    "myst_parser",
+    "sphinx_rtd_theme",
 ]
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "sphinx_rtd_theme"
-html_static_path = ['_static']
+html_static_path = ["_static"]
 source_suffix = {
-    '.rst': 'restructuredtext',
-    '.txt': 'markdown',
-    '.md': 'markdown',
+    ".rst": "restructuredtext",
+    ".txt": "markdown",
+    ".md": "markdown",
 }
