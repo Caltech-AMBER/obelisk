16b390c179d77ea6144c33314bf068cfbb02e674
Zachary <zach.olkin@gmail.com>
2024-06-19 15:38:29 -0700

    basic cpp setup with cmake and catch2 for testing

---
+++ obelisk/cpp/tests/dummy_test.cpp
@@ -0,0 +1,12 @@
+/*
+ * Obelisk Software Framework.
+ * Developed in the Amber Lab at Caltech. 
+ * Copyright (c) 2024 Amber Lab. All rights reserved.
+ */
+
+#include <catch2/catch_test_macros.hpp>
+
+TEST_CASE("Dummy test", "[test]") {
+    int i = 1;
+    REQUIRE(i == 1);
+}
