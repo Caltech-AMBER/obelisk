c9e7839db282991f9f4ff8be88e5e472a996c729
Zachary <zach.olkin@gmail.com>
2024-06-23 03:40:46 +0000

    checks the message type at run time

--- obelisk/cpp/obelisk_cpp/include/obelisk_node.h
+++ obelisk/cpp/obelisk_cpp/include/obelisk_node.h
@@ -1,5 +1,7 @@
 #pragma once
+#include <variant>
 
+#include "rcl_interfaces/msg/parameter_event.hpp"
 #include "rclcpp/rclcpp.hpp"
 #include "rclcpp_lifecycle/lifecycle_node.hpp"
 
@@ -14,11 +16,15 @@ namespace obelisk {
        public:
         explicit ObeliskNode(const std::string& node_name,
             const rclcpp::NodeOptions& options  = rclcpp::NodeOptions(),
-            bool enable_communication_interface = true);
+            bool enable_communication_interface = true)
+            : LifecycleNode(
+                  node_name, options, enable_communication_interface) {};
 
         ObeliskNode(const std::string& node_name, const std::string& namespace_,
             const rclcpp::NodeOptions& options  = rclcpp::NodeOptions(),
-            bool enable_communication_interface = true);
+            bool enable_communication_interface = true)
+            : LifecycleNode(node_name, namespace_, options,
+                  enable_communication_interface) {};
 
         template <typename MessageT, typename AllocatorT = std::allocator<void>>
         std::shared_ptr<
@@ -26,7 +32,15 @@ namespace obelisk {
         create_publisher(const std::string& topic_name, const rclcpp::QoS& qos,
             const rclcpp::PublisherOptionsWithAllocator<AllocatorT>& options =
                 (rclcpp_lifecycle::create_default_publisher_options<
-                    AllocatorT>()));
+                    AllocatorT>())) {
+            if (ValidMessage<MessageT, ObeliskMsgs>::value) {
+                return rclcpp_lifecycle::LifecycleNode::create_publisher<
+                    MessageT, AllocatorT>(topic_name, qos, options);
+            }
+
+            throw std::runtime_error(
+                "Provided message type is not a valid Obelisk message!");
+        };
 
         template <typename MessageT, typename CallbackT,
             typename AllocatorT    = std::allocator<void>,
@@ -40,7 +54,36 @@ namespace obelisk {
                 options = rclcpp_lifecycle::create_default_subscription_options<
                     AllocatorT>(),
             typename MessageMemoryStrategyT::SharedPtr msg_mem_strat =
-                (MessageMemoryStrategyT::create_default()));
-    };
+                (MessageMemoryStrategyT::create_default())) {
+            return rclcpp_lifecycle::LifecycleNode::create_subscription<
+                MessageT, CallbackT, AllocatorT, SubscriptionT,
+                MessageMemoryStrategyT>(topic_name, qos, callback, options);
+        };
 
+        template <typename scalar, typename scalar_2>
+        scalar SimpleFunc(scalar a, scalar_2 b) {
+            return a;
+        }
+
+       private:
+        using ObeliskMsgs    = std::tuple<rcl_interfaces::msg::ParameterEvent>;
+        using ROSAllowedMsgs = std::tuple<rcl_interfaces::msg::ParameterEvent>;
+
+        // Helper functions for determining if we have a valid message
+        template <typename T, typename Tuple>
+        struct has_type;
+
+        template <typename T>
+        struct has_type<T, std::tuple<>> : std::false_type {};
+
+        template <typename T, typename U, typename... Ts>
+        struct has_type<T, std::tuple<U, Ts...>>
+            : has_type<T, std::tuple<Ts...>> {};
+
+        template <typename T, typename... Ts>
+        struct has_type<T, std::tuple<T, Ts...>> : std::true_type {};
+
+        template <typename T, typename Tuple>
+        using ValidMessage = typename has_type<T, Tuple>::type;
+    };
 }  // namespace obelisk
--- tests/tests_cpp/test_node.cpp
+++ tests/tests_cpp/test_node.cpp
@@ -2,12 +2,18 @@
 #include <iostream>
 
 #include "obelisk_node.h"
+#include "std_msgs/msg/header.hpp"
+#include "std_msgs/msg/string.hpp"
 
 TEST_CASE("Obelisk Node Tests", "[obelisk_node]") {
     std::cout << "Hello3." << std::endl;
     rclcpp::init(0, nullptr);
 
-    obelisk::ObeliskNode("hello_world2");
+    rclcpp_lifecycle::LifecycleNode node("hello_world");
+    node.create_publisher<std_msgs::msg::String>("topic", 10);
+
+    obelisk::ObeliskNode node2("hello_world2");
+    node2.create_publisher<std_msgs::msg::String>("topic", 10);
 
     rclcpp::shutdown();
 
