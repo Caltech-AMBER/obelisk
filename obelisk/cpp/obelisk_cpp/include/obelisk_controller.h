6f088a00d28e29eb4255e3387988ae6848d9a987
Zachary <zach.olkin@gmail.com>
2024-06-25 02:55:03 +0000

    in progress code for the variadic template stuff

--- obelisk/cpp/obelisk_cpp/include/obelisk_controller.h
+++ obelisk/cpp/obelisk_cpp/include/obelisk_controller.h
@@ -23,6 +23,8 @@ class ObeliskController : public ObeliskNode {
                 this->get_parameter("sub_est_config_str"), std::move(callback));
     };
 
+    // I can do this one recursively with a variadic template (i.e. recurse down
+    // the stack of types and call this on the proper publisher tuple)
     void CreateControllerPublisher() {
         // Create the state estimator subscriber with the proper configuration
         control_publisher_ = CreatePublisherFromConfigStr<ControlMessageT>(
@@ -30,20 +32,118 @@ class ObeliskController : public ObeliskNode {
     };
 
     // TODO: What does the timer take?
-    template <
-        typename MessageT, typename AllocatorT = std::allocator<void>,
-        typename SubscriptionT = rclcpp::Subscription<MessageT, AllocatorT>>
-    std::shared_ptr<SubscriptionT> CreateControlTimer() {
+    template <typename CallbackT>
+    typename rclcpp::GenericTimer<CallbackT>::SharedPtr
+    CreateControlTimer(CallbackT&& callback) {
         // Create the state estimator subscriber with the proper configuration
-        return CreatePublisherFromConfigStr<MessageT>(
-            this->get_parameter("timer_ctrl_config_str"));
+        return CreateWallTimerFromConfigStr(
+            this->get_parameter("timer_ctrl_config_str"), std::move(callback));
     };
 
   protected:
+    // Hold a tuple of publishers and tuple of subscribers, defined from a
+    // variadic template
     typename rclcpp::Publisher<ControlMessageT>::SharedPtr control_publisher_;
     typename rclcpp::Subscription<StateMessageT>::SharedPtr
         state_estimator_subscriber_;
 
+    // In the same way that I can make a tuple of pubs and subs, I should be
+    // able to make a tuple of callback types Then the downstream user will need
+    // to populate the tuple with the callbacks (can be results of std::bind)
+    // Then I should be able to assign the Callback to the write function by teh
+    // get function on the tuple (because I know the type) If the tuple is empty
+    // or the get doesn't work then it means the user did not specify the call
+    // back
+
   private:
 };
 } // namespace obelisk
+
+// Chat gpt example
+// For the subscriber:
+// #include <iostream>
+// #include <typeinfo>
+
+// // Primary template class definition
+// template<typename... Types>
+// class MyClass;
+
+// // Base case: specialization for a single type
+// template<typename T>
+// class MyClass<T> {
+// public:
+//     template<typename U>
+//     void handle_type() {
+//         if (typeid(T) == typeid(U)) {
+//             std::cout << "Handling type: " << typeid(U).name() << std::endl;
+//         } else {
+//             std::cout << "Type mismatch: " << typeid(U).name() << " != " <<
+//             typeid(T).name() << std::endl;
+//         }
+//     }
+// };
+
+// // Recursive case: handle the first type and then the rest
+// template<typename T, typename... Rest>
+// class MyClass<T, Rest...> : public MyClass<Rest...> {
+// public:
+//     using MyClass<Rest...>::handle_type; // Bring the base class function
+//     into scope
+
+//     template<typename U>
+//     void handle_type() {
+//         if (typeid(T) == typeid(U)) {
+//             std::cout << "Handling type: " << typeid(U).name() << std::endl;
+//         } else {
+//             MyClass<Rest...>::template handle_type<U>();
+//         }
+//     }
+// };
+
+// int main() {
+//     MyClass<int, double, char> myClass;
+
+//     // Call handle_type for each type explicitly
+//     myClass.handle_type<int>();    // Output: Handling type: int
+//     myClass.handle_type<double>(); // Output: Handling type: double
+//     myClass.handle_type<char>();   // Output: Handling type: char
+//     myClass.handle_type<float>();  // Output: Type mismatch: float != char
+//     (since float is not part of the types)
+
+//     return 0;
+// }
+
+// For the publisher:
+// #include <iostream>
+
+// // Primary template class definition
+// template<typename... Types>
+// class MyClass;
+
+// // Base case: specialization for a single type
+// template<typename T>
+// class MyClass<T> {
+// public:
+//     void handleType() {
+//         std::cout << "Handling type: " << typeid(T).name() << std::endl;
+//     }
+// };
+
+// // Recursive case: handle the first type and then the rest
+// template<typename T, typename... Rest>
+// class MyClass<T, Rest...> : public MyClass<Rest...> {
+// public:
+//     using MyClass<Rest...>::handleType; // Bring the base class function into
+//     scope
+
+//     void handleType() {
+//         std::cout << "Handling type: " << typeid(T).name() << std::endl;
+//         MyClass<Rest...>::handleType();
+//     }
+// };
+
+// int main() {
+//     MyClass<int, double, char> myClass;
+//     myClass.handleType(); // Will call handleType for each type: int, double,
+//     char return 0;
+// }
