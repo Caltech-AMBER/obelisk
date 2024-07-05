8412047277cb585f279701ad3849050a3f8e50bc
Zach Olkin <zach.olkin@gmail.com>
2024-07-05 00:48:49 +0000

    updated the tests a little bit

--- tests/tests_cpp/claude_obelisk_estimator_test.cpp
+++ tests/tests_cpp/claude_obelisk_estimator_test.cpp
@@ -12,7 +12,6 @@ class TestObeliskEstimator : public obelisk::ObeliskEstimator<obelisk_estimator_
 
     using ObeliskEstimator::estimator_publisher_;
     using ObeliskEstimator::estimator_timer_;
-    using ObeliskEstimator::sub_sensor_config_strs_;
 };
 
 TEST_CASE("ObeliskEstimator Construction and Configuration", "[ObeliskEstimator]") {
@@ -22,8 +21,6 @@ TEST_CASE("ObeliskEstimator Construction and Configuration", "[ObeliskEstimator]
 
     estimator.set_parameter(rclcpp::Parameter("timer_est_setting", "timer_period_sec:0.1"));
     estimator.set_parameter(rclcpp::Parameter("pub_est_setting", "topic:/estimated_state,history_depth:10"));
-    // std::vector<std::string> test_configs = {"topic:/sensor1,history_depth:10", "topic:/sensor2,history_depth:20"};
-    // estimator.set_parameter(rclcpp::Parameter("sub_sensor_settings", test_configs));
 
     auto result = estimator.on_configure(rclcpp_lifecycle::State());
     REQUIRE(result == rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn::SUCCESS);
@@ -58,21 +55,18 @@ TEST_CASE("ObeliskEstimator Cleanup and Shutdown", "[ObeliskEstimator]") {
     TestObeliskEstimator estimator;
     estimator.set_parameter(rclcpp::Parameter("timer_est_setting", "timer_period_sec:0.1"));
     estimator.set_parameter(rclcpp::Parameter("pub_est_setting", "topic:/estimated_state,history_depth:10"));
-    // std::vector<std::string> test_configs = {"topic:/sensor1,history_depth:10"};
-    // estimator.set_parameter(rclcpp::Parameter("sub_sensor_settings", test_configs));
+
     estimator.on_configure(rclcpp_lifecycle::State());
 
     auto cleanup_result = estimator.on_cleanup(rclcpp_lifecycle::State());
     REQUIRE(cleanup_result == rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn::SUCCESS);
     REQUIRE(estimator.estimator_publisher_ == nullptr);
     REQUIRE(estimator.estimator_timer_ == nullptr);
-    // REQUIRE(estimator.sub_sensor_config_strs_.empty());
 
     auto shutdown_result = estimator.on_shutdown(rclcpp_lifecycle::State());
     REQUIRE(shutdown_result == rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn::SUCCESS);
     REQUIRE(estimator.estimator_publisher_ == nullptr);
     REQUIRE(estimator.estimator_timer_ == nullptr);
-    // REQUIRE(estimator.sub_sensor_config_strs_.empty());
 
     rclcpp::shutdown();
 }
@@ -83,8 +77,6 @@ TEST_CASE("ObeliskEstimator with Empty Sensor Settings", "[ObeliskEstimator]") {
     TestObeliskEstimator estimator;
     estimator.set_parameter(rclcpp::Parameter("timer_est_setting", "timer_period_sec:0.1"));
     estimator.set_parameter(rclcpp::Parameter("pub_est_setting", "topic:/estimated_state,history_depth:10"));
-    // std::vector<std::string> empty_configs = {""};
-    // estimator.set_parameter(rclcpp::Parameter("sub_sensor_setting", empty_configs));
 
     auto result = estimator.on_configure(rclcpp_lifecycle::State());
     REQUIRE(result == rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn::SUCCESS);
