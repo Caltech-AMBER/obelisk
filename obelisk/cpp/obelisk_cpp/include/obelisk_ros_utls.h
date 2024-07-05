49aff57aaddbc6125f3ed6deceda4b6bcb437cda
Zachary <zach.olkin@gmail.com>
2024-07-05 17:13:31 +0000

    remove node after spin

--- obelisk/cpp/obelisk_cpp/include/obelisk_ros_utls.h
+++ obelisk/cpp/obelisk_cpp/include/obelisk_ros_utls.h
@@ -8,12 +8,12 @@ namespace obelisk::utils {
     template <typename NodeT, typename ExecutorT> void SpinObelisk(int argc, char* argv[], const std::string& name) {
         rclcpp::init(argc, argv);
 
-        auto robot = std::make_shared<NodeT>(name);
+        auto node = std::make_shared<NodeT>(name);
 
         ExecutorT executor;
-        executor.add_node(robot->get_node_base_interface());
+        executor.add_node(node->get_node_base_interface());
         executor.spin();
-
+        executor.remove_node(node->get_node_base_interface());
         rclcpp::shutdown();
     }
 } // namespace obelisk::utils
