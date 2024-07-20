54e02ec29e979edffc3d6325ae1cc658fa0ebe4f
alberthli <alberthli@caltech.edu>
2024-07-20 15:40:12 -0700

    MujocoImage -> ObkImage

--- tests/tests_python/tests_core/test_utils/test_msg.py
+++ tests/tests_python/tests_core/test_utils/test_msg.py
@@ -6,7 +6,7 @@ from obelisk_py.core.utils.msg import multiarray_to_np, np_to_multiarray
 
 def test_conversion() -> None:
     """Test the conversion between numpy array and FloatMultiArray."""
-    arr = np.random.rand(3, 4, 5)
+    arr = np.random.rand(3, 4, 5, 6, 7)
     msg = np_to_multiarray(arr)
     assert isinstance(msg, FloatMultiArray)
     arr_recovered = multiarray_to_np(msg)
