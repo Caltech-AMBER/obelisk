7e85c57b722c0ac16ea42fac777b02217b0b6639
alberthli <alberthli@caltech.edu>
2024-08-02 06:17:37 +0000

    allow leap hand gains to be configurable from yaml

--- obelisk/cpp/zoo/hardware/robots/leap/leap_hand_interface.h
+++ obelisk/cpp/zoo/hardware/robots/leap/leap_hand_interface.h
@@ -30,9 +30,18 @@ namespace obelisk {
               packet_handler_(dynamixel::PacketHandler::getPacketHandler()),
               group_reader_(port_handler_, packet_handler_), group_writer_(port_handler_, packet_handler_),
               state_timer_key_(state_timer_key), state_pub_key_(state_pub_key) {
+
+            // exposing gains as ros params
+            this->declare_parameter("KP", 150);
+            this->declare_parameter("KI", 0);
+            this->declare_parameter("KD", 50);
+
+            // timer/pub pair for the leap hand state
             this->RegisterObkTimer("timer_sensor_setting", state_timer_key_,
                                    std::bind(&ObeliskLeapHand::PublishState, this));
             this->template RegisterObkPublisher<leap_sensor_msg>("pub_sensor_setting", state_pub_key_);
+
+            // setting up ports and initial values
             if (port_handler_->openPort()) {
                 std::cout << "Opened port\n";
             } else {
@@ -44,28 +53,6 @@ namespace obelisk {
             } else {
                 std::cerr << "Failed to set baud rate\n";
             }
-
-            const int TORQUE_ADDR       = 64;
-            const int TORQUE_ENABLE_VAL = 1;
-            const int KP_ADDR           = 84;
-            const int KI_ADDR           = 82;
-            const int KD_ADDR           = 80;
-            // TODO: expose as ros params
-            const int KP_VAL = 500;
-            const int KI_VAL = 10;
-            const int KD_VAL = 50;
-
-            for (int i = 0; i < N_MOTORS; i++) {
-                int err =
-                    packet_handler_->write1ByteTxRx(port_handler_, i, TORQUE_ADDR, TORQUE_ENABLE_VAL); // Enable torque
-                err |= packet_handler_->write2ByteTxRx(port_handler_, i, KP_ADDR, KP_VAL);             // kP
-                err |= packet_handler_->write2ByteTxRx(port_handler_, i, KI_ADDR, KI_VAL);             // kI
-                err |= packet_handler_->write2ByteTxRx(port_handler_, i, KD_ADDR, KD_VAL);             // kD
-                if (err != 0) {
-                    std::cerr << "Failed to initialize motor " << i << "\n";
-                }
-            }
-            std::cout << "Initialized motors\n";
         }
 
         /**
@@ -135,6 +122,48 @@ namespace obelisk {
         static constexpr int GOAL_POS_LENGTH    = 4;
         static constexpr int PRESENT_POS_ADDR   = 132;
         static constexpr int PRESENT_POS_LENGTH = 4;
+
+        static constexpr int TORQUE_ADDR       = 64;
+        static constexpr int TORQUE_ENABLE_VAL = 1;
+        static constexpr int KP_ADDR           = 84;
+        static constexpr int KI_ADDR           = 82;
+        static constexpr int KD_ADDR           = 80;
+
+        double KP_;
+        double KI_;
+        double KD_;
+
+        rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn
+        on_configure(const rclcpp_lifecycle::State& state) {
+            ObeliskRobot::on_configure(state);
+
+            // setting gains and activating motors
+            KP_ = this->get_parameter("KP").as_int();
+            KI_ = this->get_parameter("KI").as_int();
+            KD_ = this->get_parameter("KD").as_int();
+
+            for (int i = 0; i < N_MOTORS; i++) {
+                int err =
+                    packet_handler_->write1ByteTxRx(port_handler_, i, TORQUE_ADDR, TORQUE_ENABLE_VAL); // Enable torque
+                err |= packet_handler_->write2ByteTxRx(port_handler_, i, KP_ADDR, KP_);                // KP
+                err |= packet_handler_->write2ByteTxRx(port_handler_, i, KI_ADDR, KI_);                // KI
+                err |= packet_handler_->write2ByteTxRx(port_handler_, i, KD_ADDR, KD_);                // KD
+                if (err != 0) {
+                    RCLCPP_ERROR_STREAM(this->get_logger(), "Failed to initialize motor " << i);
+                }
+            }
+            RCLCPP_INFO_STREAM(this->get_logger(), "Initialized motors!");
+
+            // homing the LEAP hand
+            leap_control_msg home_msg  = leap_control_msg();
+            std::vector<double> q_home = {0.5, -0.75, 0.75, 0.25, 0.5,  0.0, 0.75, 0.25,
+                                          0.5, 0.75,  0.75, 0.25, 0.65, 0.9, 0.75, 0.6};
+            home_msg.q_des             = q_home;
+            ApplyControl(home_msg);
+
+            RCLCPP_INFO_STREAM(this->get_logger(), "Configured Leap Hand!");
+            return CallbackReturn::SUCCESS;
+        }
 
         static inline int RadiansToDxlPos(const double& rads) {
             return (rads + M_PI) / (2 * M_PI) * (DXL_MAX_POS - DXL_MIN_POS);
