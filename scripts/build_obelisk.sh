bb413fc935317b75034561b64241ce091ce09bc8
Zachary <zach.olkin@gmail.com>
2024-08-07 16:54:20 +0000

    changed build leap and zed if statement, added verbose flag to build

--- scripts/build_obelisk.sh
+++ scripts/build_obelisk.sh
@@ -2,6 +2,7 @@
 
 leap=false
 zed=false
+verbose=false
 
 for arg in "$@"; do
     case $arg in
@@ -14,17 +15,20 @@ for arg in "$@"; do
             zed=true
             shift  # Adds ZED ROS packages to colcon build command
             ;;
+        --verbose)
+            verbose=true
+            shift
+            ;;
         *)
             # Unknown option
             echo "Unknown option: $arg"
-            echo "Usage: $0 [--leap] [--zed]"
+            echo "Usage: $0 [--leap] [--zed] [--verbose]"
             exit 1
             ;;
     esac
 done
 
 MESSAGE_PKGS=" obelisk_control_msgs obelisk_estimator_msgs obelisk_sensor_msgs obelisk_std_msgs"
-
 # configuring which packages to skip
 SKIP_PKGS=""
 if [ "$leap" = false ]; then
@@ -40,14 +44,26 @@ OBELISK_ROOT=$(dirname $(dirname $(readlink -f ${BASH_SOURCE[0]})))
 echo -e "\033[1;32mBuilding Obelisk ROS messages...\033[0m"
 curr_dir=$(pwd)
 cd $OBELISK_ROOT/obelisk_ws
+if [ "$verbose" = true ]; then
 colcon build --symlink-install --parallel-workers $(nproc) \
-    --packages-select $MESSAGE_PKGS
+    --packages-select $MESSAGE_PKGS \
+    --event-handlers console_direct+
+else
+    colcon build --symlink-install --parallel-workers $(nproc) \
+        --packages-select $MESSAGE_PKGS
+fi
 
 # for debugging, add `--event-handlers console_direct+` to the colcon build command
 echo -e "\033[1;32mBuilding remainder of Obelisk ROS packages...\033[0m"
 source $OBELISK_ROOT/obelisk_ws/install/setup.bash
+if [ "$verbose" = true ]; then
 colcon build --symlink-install --parallel-workers $(nproc) \
-    --packages-skip $MESSAGE_PKGS $SKIP_PKGS
+    --packages-skip $MESSAGE_PKGS $SKIP_PKGS \
+    --event-handlers console_direct+
+else
+    colcon build --symlink-install --parallel-workers $(nproc) \
+        --packages-skip $MESSAGE_PKGS $SKIP_PKGS
+fi
 source $OBELISK_ROOT/obelisk_ws/install/setup.bash
 cd $curr_dir
 
