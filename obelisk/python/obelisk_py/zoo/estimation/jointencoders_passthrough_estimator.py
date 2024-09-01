20a1af91ef108239f33a0224318bbdb8170429ca
alberthli <alberthli@caltech.edu>
2024-09-01 22:56:22 +0000

    updated leap hand estimator to include v_joints

--- obelisk/python/obelisk_py/zoo/estimation/jointencoders_passthrough_estimator.py
+++ obelisk/python/obelisk_py/zoo/estimation/jointencoders_passthrough_estimator.py
@@ -19,11 +19,14 @@ class JointEncodersPassthroughEstimator(ObeliskEstimator):
             key="sub_sensor",  # key can be specified here or in the config file
             msg_type=ObkJointEncoders,
         )
+        self._last_msg = None
 
     def on_configure(self, state: LifecycleState) -> TransitionCallbackReturn:
         """Configure the estimator."""
         super().on_configure(state)
         self.joint_encoder_values = None
+        self.joint_encoder_values_prev = None
+        self.t_prev = None
         return TransitionCallbackReturn.SUCCESS
 
     def joint_encoder_callback(self, msg: ObkJointEncoders) -> None:
@@ -33,7 +36,31 @@ class JointEncodersPassthroughEstimator(ObeliskEstimator):
     def compute_state_estimate(self) -> Union[EstimatedState, None]:
         """Compute the state estimate."""
         estimated_state_msg = EstimatedState()
-        if self.joint_encoder_values is not None:
+        estimated_state_msg.header.stamp = self.get_clock().now().to_msg()
+
+        if self.joint_encoder_values is None or (self.joint_encoder_values == self.joint_encoder_values_prev):
+            if self._last_msg is not None:
+                return self._last_msg
+            else:
+                return None
+        else:
             estimated_state_msg.q_joints = self.joint_encoder_values
+            if self.joint_encoder_values_prev is not None:
+                t = self.t
+                dt = t - self.t_prev
+                estimated_state_msg.v_joints = [
+                    (q - q_prev) / dt for q, q_prev in zip(self.joint_encoder_values, self.joint_encoder_values_prev)
+                ]
+                self.joint_encoder_values_prev = self.joint_encoder_values
+                self.t_prev = t
+            else:
+                estimated_state_msg.v_joints = [0.0] * len(self.joint_encoder_values)
+                self.joint_encoder_values_prev = self.joint_encoder_values
+                self.t_prev = self.t
+            estimated_state_msg.joint_names = [
+                "if_mcp", "if_rot", "if_pip", "if_dip", "mf_mcp", "mf_rot", "mf_pip", "mf_dip",
+                "rf_mcp", "rf_rot", "rf_pip", "rf_dip", "th_cmc", "th_axl", "th_mcp", "th_ipl",
+            ]  # fmt: skip
             self.obk_publishers["pub_est"].publish(estimated_state_msg)
+            self._last_msg = estimated_state_msg
             return estimated_state_msg
