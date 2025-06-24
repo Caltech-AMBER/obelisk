74ccf7dde315b637d05d75fa9e030da2ad627972
Albert Li <alberthli@caltech.edu>
2025-06-24 12:12:30 -0700

    update example joint controller

--- obelisk/python/obelisk_py/zoo/control/example/fr3_example_joint_controller.py
+++ obelisk/python/obelisk_py/zoo/control/example/fr3_example_joint_controller.py
@@ -15,6 +15,15 @@ class FR3ExampleJointController(ObeliskController):
     def __init__(self, node_name: str = "fr3_example_joint_controller") -> None:
         """Initialize the example fr3 joint controller."""
         super().__init__(node_name, PositionSetpoint, EstimatedState)
+        self.reset_pos = [
+            0.09162008114028396,
+            -0.19826458111314524,
+            -0.01990020486871322,
+            -2.4732269941140346,
+            -0.01307073642274261,
+            2.30396583422025,
+            0.8480939705504309,
+        ]
 
     def on_configure(self, state: LifecycleState) -> TransitionCallbackReturn:
         """Configure the controller."""
@@ -36,10 +45,11 @@ class FR3ExampleJointController(ObeliskController):
         Returns:
             The control message.
         """
-        # setting the message
         position_setpoint_msg = PositionSetpoint()
-        position_setpoint_msg.u_mujoco = [(0.3 * np.sin(self.t)) for _ in range(7)]  # example state-independent input
-        position_setpoint_msg.q_des = [(0.3 * np.sin(self.t)) for _ in range(7)]  # example state-independent input
+
+        # example state-independent input
+        position_setpoint_msg.u_mujoco = [self.reset_pos[i] + 0.3 * np.sin(self.t) for i in range(7)]
+        position_setpoint_msg.q_des = [self.reset_pos[i] + 0.3 * np.sin(self.t) for i in range(7)]
         self.obk_publishers["pub_ctrl"].publish(position_setpoint_msg)
         assert is_in_bound(type(position_setpoint_msg), ObeliskControlMsg)
         return position_setpoint_msg  # type: ignore
