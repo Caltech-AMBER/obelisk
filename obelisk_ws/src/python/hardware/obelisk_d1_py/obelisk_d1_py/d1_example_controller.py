d8383e0eba665d9fdbaaeb6054eeb257b2bd95bc
Eloise Zeng <eloise.zeng@gmail.com>
2025-06-25 12:00:49 -0700

    minor edits

--- obelisk_ws/src/python/hardware/obelisk_d1_py/obelisk_d1_py/d1_example_controller.py
+++ obelisk_ws/src/python/hardware/obelisk_d1_py/obelisk_d1_py/d1_example_controller.py
@@ -3,6 +3,7 @@ from typing import List, Optional
 from rclpy.executors import SingleThreadedExecutor
 
 from obelisk_py.core.utils.ros import spin_obelisk
+from obelisk_py.zoo.control.example.example_position_setpoint_controller import ExamplePositionSetpointController
 from obelisk_py.zoo.control.example.d1_controller import D1Controller
 
 def main(args: Optional[List] = None) -> None:
