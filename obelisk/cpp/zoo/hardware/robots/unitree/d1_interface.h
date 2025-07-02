a51176f4256acec8ae751ef2db762662686fd178
eloisezeng <eloise.zeng@gmail.com>
2025-07-01 17:29:39 -0700

    code runs

--- obelisk/cpp/zoo/hardware/robots/unitree/d1_interface.h
+++ obelisk/cpp/zoo/hardware/robots/unitree/d1_interface.h
@@ -10,7 +10,6 @@
 
 // Message Types
 #include <msg/ArmString_.hpp>
-#include <msg/PubServoInfo_.hpp>
 
 namespace obelisk {
     using namespace unitree::common;
@@ -18,20 +17,20 @@ namespace obelisk {
     using namespace unitree_arm::msg::dds_;
     
     using d1_control_msg = obelisk_control_msgs::msg::PositionSetpoint;
-    using d1_sensor_msg = obelisk_sensor_msgs::msg::ObkJointEncoders;
+    using d1_sensor_msg = obelisk_sensor_msgs::msg::ObkJointEncoders; // TODO: Do something with this?
 
     class D1Interface : public ObeliskRobot<d1_control_msg> {
         public:
             D1Interface(const std::string& node_name)
                 : ObeliskRobot<d1_control_msg>(node_name) {
-
+                RCLCPP_INFO_STREAM(this->get_logger(), "Initializing Interface");
                 // Get network interface name as a parameter
                 this->declare_parameter<std::string>("network_interface_name", "");
                 network_interface_name_ = this->get_parameter("network_interface_name").as_string();
                 RCLCPP_INFO_STREAM(this->get_logger(), "Network interface: " << network_interface_name_);
 
                 ChannelFactory::Instance()->Init(0, network_interface_name_);
-
+                
                 CreatePublishers();
             }
 
@@ -44,8 +43,10 @@ namespace obelisk {
             void CreateSubscribers() {
                 // Create subscribers
                 // Should create after the publishers have been activated
-                lowstate_subscriber_.reset(new ChannelSubscriber<PubServoInfo_>(STATE_TOPIC));
-                // lowstate_subscriber.InitChannel(Handler); // TODO
+                RCLCPP_INFO_STREAM(this->get_logger(), "Creating subscribers");
+                lowstate_subscriber_.reset(new ChannelSubscriber<ArmString_>(STATE_TOPIC));
+                lowstate_subscriber_->InitChannel(std::bind(&D1Interface::LowStateHandler, this, std::placeholders::_1));
+                RCLCPP_INFO_STREAM(this->get_logger(), "Subscribed to " << STATE_TOPIC);
             }
 
             rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn virtual on_activate(
@@ -57,36 +58,67 @@ namespace obelisk {
                 return rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn::SUCCESS;
             }
 
+            void LowStateHandler(const void* msg) {
+                /*
+                Called when the arm_Feedback subscriber hears a message on the state topic.
+                
+                The joint positions are in degrees.
+                */
+                const ArmString_* pm = (const ArmString_*) msg;
+                RCLCPP_INFO_STREAM(this->get_logger(), "arm_Feedback_data:" << pm->data_());
+            }
+
             void ApplyControl(const d1_control_msg& control_msg) {
                 /*
                 Command the joint positions of the D1 Arm.
 
                 The D1 Arm can also be commanded to release control of all motors, but this is not implemented here.
                 */
-                // for (int i = 0; i < NUM_JOINTS; i++) {
-                //     pos_in_rads = control_msg.q_des.at(i);
-                //     pos_in_degrees = pos_in_rads * 180 / M_PI;
-                // }
+                std::stringstream cmd;
+                cmd << "{\"seq\":" 
+                << SEQ_ALL_JOINTS 
+                << ",\"address\":" 
+                << ADDRESS_ALL_JOINTS 
+                << ",\"funcode\":" 
+                << FUNCODE_ALL_JOINTS 
+                << ",\"data\":{\"mode\":"
+                << MODE_ALL_JOINTS;
+
+                double pos_in_rads;
+                double pos_in_degrees;
+
+                for (int i = 0; i < NUM_JOINTS; i++) {
+                    pos_in_rads = control_msg.q_des.at(i);
+                    pos_in_degrees = pos_in_rads * 180 / M_PI;
+                    // Round to two decimal places
+                    pos_in_degrees = std::round(pos_in_degrees * 100) / 100;
+                    cmd << ",\"angle" << i << "\":" << pos_in_degrees;
+                }
+                
+                cmd << "}}";
+                std::string cmd_str = cmd.str();
+                // RCLCPP_INFO_STREAM(this->get_logger(), "cmd_str: " << cmd_str);
 
                 // Create message
                 ArmString_ msg{};
-                msg.data_() = "{\"seq\":4,\"address\":1,\"funcode\":2,\"data\":{\"mode\":1,\"angle0\":0,\"angle1\":-60,\"angle2\":60,\"angle3\":0,\"angle4\":30,\"angle5\":0,\"angle6\":0}}";
-                
+                msg.data_() = cmd_str;
+
                 // Publish message
                 lowcmd_publisher_->Write(msg);
             }
         
         std::string network_interface_name_;
         ChannelPublisherPtr<ArmString_> lowcmd_publisher_;
-        ChannelSubscriberPtr<PubServoInfo_> lowstate_subscriber_;
+        ChannelSubscriberPtr<ArmString_> lowstate_subscriber_;
 
         const std::string CMD_TOPIC = "rt/arm_Command";
-        const std::string STATE_TOPIC = "rt/current_servo_angle";
+        const std::string STATE_TOPIC = "rt/arm_Feedback";
+        
         static const int NUM_JOINTS = 7;
         static const int SEQ_ALL_JOINTS = 4;
         static const int ADDRESS_ALL_JOINTS = 1;
         static const int FUNCODE_ALL_JOINTS = 2;
-
+        static const int MODE_ALL_JOINTS = 1;
     };
 }
 
