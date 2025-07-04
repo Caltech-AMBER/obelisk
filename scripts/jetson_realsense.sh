4143f37504216f5155327d89b357a0e88b404b69
Lizhi Yang <lzyang@caltech.edu>
2025-07-04 16:07:36 -0700

    Update jetson_realsense.sh

--- scripts/jetson_realsense.sh
+++ scripts/jetson_realsense.sh
@@ -42,7 +42,7 @@ echo Install udev-rules
 sudo cp config/99-realsense-libusb.rules /etc/udev/rules.d/ 
 sudo /lib/systemd/systemd-udevd --dameon && sudo udevadm control --reload-rules && sudo udevadm trigger 
 mkdir build && cd build
-cmake ../ -DCMAKE_CUDA_COMPILER=/usr/local/cuda/bin/nvcc -DFORCE_LIBUVC=true -DBUILD_WITH_CUDA=true -DCMAKE_BUILD_TYPE=release
+cmake ../ -DFORCE_LIBUVC=true -DBUILD_WITH_CUDA=true -DCMAKE_BUILD_TYPE=release
 make -j2
 sudo make install
 echo -e "\e[92m\n\e[1mLibrealsense script completed.\n\e[0m"
